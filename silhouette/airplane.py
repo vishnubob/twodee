75ffc494f8e01c05ce28ab8366ceb18f22647801
Giles Hall <giles@polymerase.org>
2013-12-31 02:03:52 -0500

    airplane go!

--- silhouette/airplane.py
+++ silhouette/airplane.py
@@ -5,44 +5,46 @@ import svgwrite
 from svgwrite.mixins import Presentation
 
 class Airplane(object):
-    def __init__(self, height=8.5, width=11.0, scale=100):
-        self.height = height * scale
-        self.width = width * scale
+    def __init__(self, height=8.5, width=11.0, dpi=72):
+        self.dpi = dpi
+        self.height = height * self.dpi
+        self.width = width * self.dpi
 
     def get_origin(self):
         return (0, self.height / 2.0)
 
     def get_coord(self, step):
         angle = 90 - (11.25 * (step + 1))
-        x = math.cos(math.radians(angle))
-        y = math.sin(math.radians(angle))
-        if angle < 45 and angle > -45:
-            print "X is constant"
-            _y = self.height - self.width * (y / x) + (self.height / 2.0)
-            coord = (self.width, _y)
-        else:
-            print "Y is constant"
-            _x = self.height * (x / y)
-            if (_x < 0):
-                coord = (_x, self.height)
+        if angle == 0:
+            return (self.width, self.height / 2.0)
+        if angle < 0:
+            angle += 360
+        slope = math.tan(math.radians(angle))
+        _y = (slope * self.width) + (self.height / 2.0)
+        _x = (1 / slope) * (self.height / 2.0)
+        _x = abs(_x)
+        if _x < self.width:
+            if (_y > 0):
+                return (_x, 0)
             else:
-                coord = (_x, 0)
-        print angle, (x, y), coord
-        return coord
+                return (_x, self.height)
+        else:
+            return (self.width, _y)
 
     def generate(self, svgfn="airplane.svg", profile="tiny"):
-        dwg = svgwrite.Drawing(svgfn, profile=profile)
+        inch_width = "%.2fin" % (self.width / self.dpi)
+        inch_height = "%.2fin" % (self.height / self.dpi)
+        dwg = svgwrite.Drawing(svgfn, size=(inch_width, inch_height), profile=profile)
         origin = self.get_origin()
-        print self.width, self.height
-        print origin
         end = (self.width, origin[1])
-        for x in range(5):
+        #ln = dwg.line((self.width, 0), (self.width, self.height), stroke="black")
+        #dwg.add(ln)
+        for x in range(15):
             point = self.get_coord(x)
             ln = dwg.line(origin, point, stroke="black")
             dwg.add(ln)
         dwg.save()
 
 if __name__ == "__main__":
-    star = Airplane()
-    star.generate()
-
+    airplane = Airplane(12, 12)
+    airplane.generate()
